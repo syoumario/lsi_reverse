28abb51a8eea92e707146f9d345db9a5d420bdd2
katsuyama_notePC <katsuyama.shu@gmail.com>
2021-10-22 12:16:34 +0900

    netクラスをファイル化した。

--- LSI_dqn_osero.py
+++ LSI_dqn_osero.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import random
 import copy
 from Board.board import Board
+from Net.net import net
 from collections import namedtuple
 
 # マスの状態
@@ -12,137 +13,15 @@ EMPTY = 0 # 空きマス
 WHITE = -1 # 白石
 BLACK = 1 # 黒石
 WALL = 2 # 壁
- 
-# ボードのサイズ
-BOARD_SIZE = 4
 
-# 方向(2進数)
-NONE = 0
-LEFT = 2**0 # =1
-UPPER_LEFT = 2**1 # =2 
-UPPER = 2**2 # =4
-UPPER_RIGHT = 2**3 # =8
-RIGHT = 2**4 # =16
-LOWER_RIGHT = 2**5 # =32
-LOWER = 2**6 # =64
-LOWER_LEFT = 2**7 # =128
- 
 # 手の表現
 IN_ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 IN_NUMBER = ['1', '2', '3', '4', '5', '6', '7', '8']
  
-# 手数の上限
-MAX_TURNS = 60
+# ボードのサイズ
+BOARD_SIZE = 4
 
-
-def sigmoid(x):
-    return 1/(1 + np.exp(-x))
-
-def del_sigmoid(x):
-    return sigmoid(x) * (1 - sigmoid(x))
-
-def tanh(x):
-  return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
-
-def del_tanh(x):
-  return 1.0 - (tanh(x) ** 2)
-
-class net():
-
-    # 順伝播計算：入力から出力までを計算する。
-    # 入力x：例[1,2,3]
-    # 重みw2,w3：例それぞれ[[1,2,3],[4,5,6]] ※重みの列数が出力の数
-    def ForwardPropagation(self,x,w2,w3):
-        # 重みと内積するために、転置させて一列にする
-        # 分からんけど、最初の行に新たに行[1,1,..]を挿入する。-----なぜかを聞く！
-        z1 = np.insert(np.array([x]).T,0,1,axis=0)
-
-        #　重みと入力の内積をとる。出力例：[?,?]の１行
-        u2 = np.dot(w2,z1)
-
-        # シグモイド関数でu2:0~1に変換して、転置させて１列にする
-        z2 = np.insert(sigmoid(u2),0,1,axis=0)
-
-        # 重みとの内積をとる。出力例：[?,?]の１行
-        u3 = np.dot(w3,z2)
-
-        z3 = u3
-
-        # 辞書生成：{'z1':z1,'z2':z2,'z3':z3,'u2':u2}
-        return dict(z1=z1,z2=z2,z3=z3,u2=u2)
-
-    # 誤差逆伝播法：順伝搬計算の返値から、誤差関数の微分を返す
-    # 教師ｙ：出力と同様な形のリストor配列：例：[?,?]
-    # 他：各係数行列
-    def BackPropagation(self,y,w2,w3,z1,z2,z3,u2):
-        # 出力と教師の誤差を計算
-        d3 = (z3 - np.array([y]).T).T
-
-        #　シグモイドの場合の誤差逆伝播法における勾配式
-        d2 = np.dot(d3,w3)[:,1:]*del_sigmoid(u2).T
-
-        #
-        dw3 = d3.T*z2.T
-
-        #
-        dw2 = d2.T*z1.T
-
-        # 勾配を辞書で返す
-        return dict(dw2=dw2,dw3=dw3,d3=d3)
-
-    # 確率的勾配降下法：誤差逆伝播法で計算された計数行列の勾配を利用して計数の更新を行う
-    def decent(self,x,y,w2,w3,epsilon):
-        # 順伝播計算をして、fに係数を辞書る。
-        f = self.ForwardPropagation(x,w2,w3)
-
-        # 誤差逆伝播法により、bに勾配を辞書る
-        b = self.BackPropagation(y,w2,w3,f['z1'],f['z2'],f['z3'],f['u2'])
-
-        # 勾配に基づいて重みを更新
-        # 学習率epsilon：係数をどの程度更新するか
-        w2 = w2 - epsilon*b['dw2']
-        w3 = w3 - epsilon*b['dw3']
-
-        # １順で更新された重みを辞書で返す
-        return dict(w2=w2,w3=w3,d3=b['d3'])
-
-    # 係数の初期化と学習の実行
-    # 入力X,出力Y：リストor配列：例[[1,2,3],[2,3,4],...]
-    # n2：中間層のユニット数：ニューロン数：
-    def train(self,X,Y,n2,epoch,epsilon):
-        # 教師の１行の列数分の長さ：例では3
-        n1 = len(X[0])
-        n3 = len(Y[0])
-
-        # 教師の列数、ユニット数に合わせた次元の重みを初期値（平均０、標準偏差１の正規分布）で作る
-        w2 = np.random.normal(0,1,(n2,n1))
-
-        # 各行の列の最初に、列を引き延ばして０を入れる。-----上と合わせてなぜかを聞く！
-        w2 = np.insert(w2,0,0,axis=1)
-
-        w3 = np.random.normal(0,1,(n3,n2))
-        w3 = np.insert(w3,0,0,axis=1)
-
-        # 学習実行
-        for _ in range(epoch):
-            for x,y in zip(X,Y):
-                w = decent(x,y,w2,w3,epsilon)
-                w2 = w['w2']
-                w3 = w['w3']
-
-        # 学習結果を辞書で返す
-        return dict(w2=w2,w3=w3)
-
-    # 学習済み係数を用いて予測
-    def predict(self,x,w2,w3):
-        # 学習済み係数で順伝搬計算
-        f = self.ForwardPropagation(x,w2,w3)
-
-        # 辞書f内の出力z3を返す
-        return f['z3']
-    
-
-
+# 経験保存時の名前
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 # 経験を保存するメモリクラス
@@ -166,72 +45,6 @@ class ExperienceMemory():
     def __len__(self):
         return len(self.memory)
 
-# 実験
-    # def test():
-    #     def f(x):
-    #         return (x-1)**2
-        
-    #     # サンプルの入力データ
-    #     X = np.random.uniform(0,2,50)
-
-    #     # 標準偏差0.1、平均0の微妙な誤差を与えた教師データ
-    #     Y = f(X) + np.random.normal(0,0.1,50)
-        
-    #     # 転置させて、縦で観た方がイメージしやすいと思う
-    #     X_train = np.array([X]).T
-    #     Y_train = np.array([Y]).T
-
-    #     # 学習の実行
-    #     w = train(X_train,Y_train,5,5000,0.01)
-    #     print('学習終了')
-
-    #     # 要素数が等差数列となる配列を生成
-    #     X_test = np.linspace(0,2,100)
-
-    #     # X_testの要素数分を予測
-    #     Y_test = [predict(x,w['w2'],w['w3']) for x in X_test]
-    #     plt.plot(X_test,Y_test)
-    #     plt.show()
-    #     def f(x):
-    #     return (x-1)**2
-    
-    # # サンプルの入力データ
-    # X = np.random.uniform(0,2,50)
-
-    # # 標準偏差0.1、平均0の微妙な誤差を与えた教師データ
-    # Y = f(X) + np.random.normal(0,0.1,50)
-    
-    # # 転置させて、縦で観た方がイメージしやすいと思う
-    # X_train = np.array([X]).T
-    # Y_train = np.array([Y]).T
-
-    
-    # # --------------学習の実行--------------
-    # epoch = 200
-    # epsilon = 0.01
-    # n1 = len(X_train[0])
-    # n3 = len(Y_train[0])
-    # w2 = np.random.normal(0,1,(n2,n1))
-    # w2 = np.insert(w2,0,0,axis=1)
-    # w3 = np.random.normal(0,1,(n3,n2))
-    # w3 = np.insert(w3,0,0,axis=1)
-    # for _ in range(epoch):
-    #     for x,y in zip(X_train,Y_train):
-    #         w = decent(x,y,w2,w3,epsilon)
-    #         w2 = w['w2']
-    #         w3 = w['w3']
-    # r = dict(w2=w2,w3=w3)
-    # # --------------学習の実行--------------
-
-    # # 要素数が等差数列となる配列を生成
-    # X_test = np.linspace(0,2,100)
-
-    # # X_testの要素数分を予測
-    # Y_test = [predict(x,r['w2'],w['w3']) for x in X_test]
-    # plt.plot(X_test,Y_test)
-    # plt.show()
-
-
 # class Board() のRawBoardをIN = [0] * (BOARD_SIZE * BOARD_SIZE)の形に変換
 def trans(RawBoard_t):
     count = 0
@@ -250,9 +63,10 @@ def to_osero():
     OUT = [0] * (BOARD_SIZE * BOARD_SIZE) # Q値
     ACT = np.arange(BOARD_SIZE * BOARD_SIZE) # 行動：[0,1,2,3,...]
 
+    # クラスのインスタンス化
     Target_network = net()
     Q_network = net()
-    board = Board() 
+    board = Board(BOARD_SIZE) 
     memory = ExperienceMemory(100)
 
     print(trans(board.RawBoard))
@@ -265,11 +79,7 @@ def to_osero():
     w2 = np.insert(w2,0,0,axis=1)
     w3 = np.random.normal(0,1,(n3,n2))
     w3 = np.insert(w3,0,0,axis=1)
-
-    w2_init = w2
-    w3_init = w3
     # -----------------------------------
-
 
     buf_action = 0
     NB_EPISODE = 2000
@@ -429,20 +239,21 @@ def to_osero():
                 # 5.（定期動作）Experience Bufferから任意の経験を取り出し、Q Networkをミニバッチ学習(Experience Replay)
 
         #ボードの初期化
-        board.__init__()
+        board.__init__(BOARD_SIZE)
         # 5.（定期動作）Experience Bufferから任意の経験を取り出し、Q Networkをミニバッチ学習(Experience Replay)
-        # 定期的に貯めた経験で学習？手法は分かんない。学習に合わせて、保存方法もチェック
+        w2_init = w2 # Target_network用に重みを固定
+        w3_init = w3 # Target_network用に重みを固定
         if episode % 100 == 0 and episode != 0:
-            #print(episode)
-            # print(len(memory.memory))
-            # print(memory.memory)
-            # print(memory.capacity)
-            # print(memory.index)
-            data = memory.sample(80)
-            # Q Networkを学習
-            for _ in range(20):
-                for i in range(0,80):
-                    buf_data = data[i]
+            # 経験をランダムサンプリング
+            memory_num = 80
+            data = memory.sample(memory_num)
+            # Q Networkの学習実行：dataからミニバッチ法でやりたい
+            Epoch_Q = 200
+            Bach_Size_Q = 40
+            for _ in range(Epoch_Q):
+                Random_index = random.sample(range(memory_num), k=Bach_Size_Q)
+                for i in range(0,Bach_Size_Q):
+                    buf_data = data[Random_index[i]]
                     # buf = Q_network.ForwardPropagation(buf_data.state, w2, w3)
                     # # x = buf['z3'][buf_data.action]
                     # x = buf['z3']
